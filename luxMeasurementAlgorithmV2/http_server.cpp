28879c585d16c3c79f2590ecba236bdca460fdec
MarkoVasic02 <vasicmarko390@gmail.com>
2024-10-08 21:15:43 +0200

    Add listFiles method to handle /files

--- luxMeasurementAlgorithmV2/http_server.cpp
+++ luxMeasurementAlgorithmV2/http_server.cpp
@@ -16,7 +16,20 @@ void handleFileDownload() {
     }
 }
 
+void listFiles() {
+    String fileList = "<html><body><h1>Available Files</h1><ul>";
+    File root = SPIFFS.open("/");
+    File file = root.openNextFile();
+    while (file) {
+        fileList += "<li><a href=\"/download?file=" + String(file.name()) + "\">" + String(file.name()) + "</a></li>";
+        file = root.openNextFile();
+    }
+    fileList += "</ul></body></html>";
+    server.send(200, "text/html", fileList);
+}
+
 void startHTTPServer() {
+    server.on("/files", HTTP_GET, listFiles);
     server.on("/download", handleFileDownload);
     server.begin();
     Serial.println("HTTP server started.");
